04f52d4d82170d5af9785ed43f7b3b485ddf54f6
NicolasSanna <nico13sanna@gmail.com>
2024-02-03 20:13:32 +0100

    fix/ViewTodo

--- obj/Debug/net7.0-windows10.0.19041.0/win10-x64/TodoListMauiApp.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows10.0.19041.0/win10-x64/TodoListMauiApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoListMauiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+dade01b9f4cb1aef099b16c46a7100b5e92194b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoListMauiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoListMauiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
