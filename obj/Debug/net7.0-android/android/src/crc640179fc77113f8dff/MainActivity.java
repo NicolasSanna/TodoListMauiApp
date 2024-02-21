a4daaaf4517297252755f7df8353a32b1761c2ec
NicolasSanna <nico13sanna@gmail.com>
2024-02-21 18:17:38 +0100

    Ajout d'éléments dynamique sur le formulaire d'ajout

---
+++ obj/Debug/net7.0-android/android/src/crc640179fc77113f8dff/MainActivity.java
@@ -0,0 +1,48 @@
+package crc640179fc77113f8dff;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("TodoListMauiApp.MainActivity, TodoListMauiApp", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("TodoListMauiApp.MainActivity, TodoListMauiApp", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("TodoListMauiApp.MainActivity, TodoListMauiApp", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
